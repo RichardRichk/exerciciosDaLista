f494157ba998d94ba171e481d5fc74b3e8daf0ab
Richard Fernandes Passarelli <45811737874@info.local>
2023-04-14 16:00:05 -0300

    Finalizada a lista

--- Exercicio-6/Program.cs
+++ Exercicio-6/Program.cs
@@ -2,3 +2,26 @@
 // lidos em um vetor. Após isto, o algoritmo deve permitir a leitura de mais 1 nome qualquer de
 // pessoa (para efetuar uma busca) e depois escrever a mensagem ACHEI, se o nome estiver
 // entre os 10 nomes lidos anteriormente (guardados no vetor), ou NÃO ACHEI caso contrário.
+
+
+string[] nomes = new string[10];
+
+string nomeDigitado;
+
+for (var i = 0; i < 10; i++)
+{
+    Console.WriteLine($"Ola, digite o {i+1}º nome:");
+    nomes[i] = Console.ReadLine();
+}
+
+Console.WriteLine($"Certo, escreva um nome igual ou diferente dos anteriores:");
+nomeDigitado = Console.ReadLine();
+
+if (nomes.Contains(nomeDigitado))
+{
+    Console.WriteLine($"ACHEI");
+}
+else
+{
+    Console.WriteLine($"Não encontrado");  
+}
--- Exercicio-7/Program.cs
+++ Exercicio-7/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Faça um algoritmo para ler 15 números e armazenar em um vetor. Após a leitura total dos
+// 15 números, o algoritmo deve escrever esses 15 números lidos na ordem inversa da qual foi
+// declarado.
+
+float[] numeros = new float[15];
+
+for (var i = 0; i < 15; i++)
+{
+    Console.WriteLine($"Digite o {i+1}º numero:");
+    numeros[i] = float.Parse(Console.ReadLine());    
+}
+
+Console.WriteLine($"Agora vamos mostrar os numeros em ordem inversa a digitada:");
+
+for (var i = 14; i >= 0; i--)
+{
+    Console.WriteLine($"{numeros[i]}");  
+}
